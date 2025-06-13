a2f0000c04e1314f25c7c779406342ffa5bc4ac2
ShahiCrafts <dev.shahi.apps@gmail.com>
2025-06-14 00:06:16 +0545

    Final Commit Sprint/01

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppRouter from './routes/AppRouter.jsx'
+import AuthProvider from './auth/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              fontSize: '15px',
+              padding: '12px 20px',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </AuthProvider>
+  </StrictMode>
 )
